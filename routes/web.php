86dd750233e63bbb69b8f83ec1c0b67f5ed53bae
wakhid <wakhidwardani23@gmail.com>
2025-05-20 20:08:11 +0700

    feat: slicing subscription plan page

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
         return Inertia::render('Prototype/Dashboard');
     })->name('dashboard');
 
+    Route::get('/subscriptionPlan', function () {
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
+
     
 });
 
