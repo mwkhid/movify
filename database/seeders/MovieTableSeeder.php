b3eff2b3aa5e91d487f133cc7ebd309feaee020b
wakhid <wakhidwardani23@gmail.com>
2025-06-10 20:38:14 +0700

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -20,7 +20,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Action',
                 'video_url' => 'https://www.youtube.com/watch?v=mqqft2x_Aa4',
                 'thumbnail' => 'https://m.media-amazon.com/images/S/pv-target-images/3de84cca07fc963b66a01a5465c2638066119711e89c707ce952555783dd4b4f.jpg',
-                'rating' => 9.0,
+                'rating' => 5.0,
                 'is_featured' => false
             ],
             [
@@ -29,7 +29,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Action',
                 'video_url' => 'https://www.youtube.com/watch?v=Ke1Y3P9D0Bc',
                 'thumbnail' => 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p9259486_p_v13_aa.jpg',
-                'rating' => 9.2,
+                'rating' => 4.2,
                 'is_featured' => true
             ],
             [
@@ -38,7 +38,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Anime',
                 'video_url' => 'https://www.youtube.com/watch?v=5pTcio2hTSw',
                 'thumbnail' => 'https://upload.wikimedia.org/wikipedia/id/7/7f/Suzume_no_Tojimari_poster.jpg',
-                'rating' => 8.7,
+                'rating' => 4.7,
                 'is_featured' => false
             ],
         ];
--- resources/js/Layouts/Authenticated/Topbar.jsx
+++ resources/js/Layouts/Authenticated/Topbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
+import { Link } from "@inertiajs/react";
 
-export default function Topbar() {
+export default function Topbar({ name }) {
     const [dropdownOpen, setDropdownOpen] = useState(true);
     const dropdownTarget = useRef();
 
@@ -18,7 +19,7 @@ export default function Topbar() {
             <input type="text" className="top-search" placeholder="Search movie, cast, genre"
             />
             <div className="flex items-center gap-4">
-                <span className="text-black text-sm font-medium">Welcome, Granola Sky</span>
+                <span className="text-black text-sm font-medium">Welcome, {name}</span>
                 <div className="collapsible-dropdown flex flex-col gap-2 relative">
                     <div className="outline outline-2 outline-gray-2 p-[5px] rounded-full w-[60px] dropdown-button" onClick={triggerDropdown}>
                         <img src="/images/avatar.png" className="rounded-full object-cover w-full" alt="" />
@@ -27,7 +28,13 @@ export default function Topbar() {
                         ref={dropdownTarget}>
                         <a href="#!" className="transition-all hover:bg-sky-100 p-4">Dashboard</a>
                         <a href="#!" className="transition-all hover:bg-sky-100 p-4">Settings</a>
-                        <a href="sign_in.html" className="transition-all hover:bg-sky-100 p-4">Sign Out</a>
+                        <Link
+                            href={route('logout')}
+                            method="post"
+                            className="transition-all hover:bg-sky-100 p-4 text-left"
+                        >
+                            Sign Out
+                        </Link>
                     </div>
                 </div>
             </div>
